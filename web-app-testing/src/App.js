22be5fe758168cc103d1759a82c3ea8bcbfe30d7
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-10-31 17:15:33 -0400

    readme and edit app.js

---
+++ web-app-testing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Take me out to the ball game! ⚾️</h1>
+    </div>
+  );
+}
+
+export default App;
