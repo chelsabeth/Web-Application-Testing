e842eab55810bfec9c7a5012ef96399d8fc139f7
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-10-31 20:19:22 -0400

    trying to figure this out

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// created an "isolated" function -aka unit- that is easy to test!
+export const sum = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+
 function App() {
   return (
     <div className="App">
--- web-app-testing/src/App.test.js
+++ web-app-testing/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from "@testing-library/react";
+import App, { sum, subtract}  from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+// Test the sum "unit" function 
+ test("it returns the sum of two numbers", () => {
+   expect(sum(2, 2)).toBe(4);
+ })
+
+ test("it returns the difference of two numbers", () => {
+   expect(subtract(5, 3)).toBe(2);
+ })
+
+test('renders without crashing', () => {
+  render(<App />);
 });
